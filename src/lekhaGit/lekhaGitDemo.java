dbd38f53b2da45ca6ad4ac9684efd63e29a37986
lekha0519 <157812672+lekha0519@users.noreply.github.com>
2024-01-26 22:53:25 +0530

    Update lekhaGitDemo.java
    Modifying commit message

--- src/lekhaGit/lekhaGitDemo.java
+++ src/lekhaGit/lekhaGitDemo.java
@@ -4,7 +4,7 @@ public class lekhaGitDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi All, Welcome to git");
+		System.out.println("Hi Everyone, Welcome to git");
 	}
 
 }
