a686611245f0711ad9298e7dc61391651ef21f2a
sumasree <saisumasree.atmuri@gmail.com>
2024-01-26 19:32:48 +0530

    first commit

---
+++ src/lekhaGit/lekhaGitDemo.java
@@ -0,0 +1,10 @@
+package lekhaGit;
+
+public class lekhaGitDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi All, Welcome to git");
+	}
+
+}
